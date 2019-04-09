f084d7bde3ea5a09a19b6cae8aa5f951921ab5ed
aspectum <drogpe@gmail.com>
2019-04-09 11:52:02 -0300

    Gera a matriz de calibração (não sei se certo)

--- req2.py
+++ req2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 windowName = 'Janela'
 board_h = 6
@@ -6,6 +7,13 @@ board_w = 8
 board_sz = (board_w, board_h)
 n_boards = 5
 board_total = board_w * board_h
+frame_step = 10
+
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
+
+image_points = []
+object_points = []
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -14,8 +22,39 @@ def main():
     cv2.namedWindow(windowName)
     
     successes = 0
+    frame_count = 0
 
-    # while successes < n_boards:
+    while successes < n_boards:
+        ret, frame = cap.read()
+        frame_count += 1
+        if frame_count % frame_step == 0:
+            patternWasFound, corners = cv2.findChessboardCorners(frame, board_sz)
+            
+            if patternWasFound:
+                frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # find cornerSubPix??
+
+                frame_display = cv2.drawChessboardCorners(frame, board_sz, corners, patternWasFound)
+
+                if len(corners) == board_total:
+                    image_points.append(corners)
+                    object_points.append(objp)
+                    successes += 1
+            else:
+                frame_display = frame.copy()
+        else:
+            frame_display = frame.copy()
+        if cv2.waitKey(25) & 0xFF == ord('q'): 
+            break
+        cv2.imshow(windowName, frame)
+    retVal, instrinsic_matrix, distortion_matrix, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, frame.shape[:-1], None, None)
+
+    print(instrinsic_matrix)
+    print('-----------')
+    print(distortion_matrix)
+
+
+    # while cap.isOpened():
     #     ret, frame = cap.read()
     #     patternWasFound, corners = cv2.findChessboardCorners(frame, board_sz)
 
@@ -24,20 +63,10 @@ def main():
 
     #     frame_display = cv2.drawChessboardCorners(frame, board_sz, corners, patternWasFound)
 
+    #     cv2.imshow(windowName, frame_display)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        patternWasFound, corners = cv2.findChessboardCorners(frame, board_sz)
-
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # find cornerSubPix??
-
-        frame_display = cv2.drawChessboardCorners(frame, board_sz, corners, patternWasFound)
-
-        cv2.imshow(windowName, frame_display)
-
-        if cv2.waitKey(25) & 0xFF == ord('q'): 
-            break
+    #     if cv2.waitKey(25) & 0xFF == ord('q'): 
+    #         break
     
     cv2.destroyAllWindows()
 
