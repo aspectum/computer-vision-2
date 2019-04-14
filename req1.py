ce3dae75f4ee2cfb354dc335abf0e94cd3463b93
aspectum <drogpe@gmail.com>
2019-04-14 17:29:15 -0300

    Limpeza

--- req1.py
+++ req1.py
@@ -3,41 +3,35 @@ import numpy as np
 import math
 
 class janela:
-	def __init__(self, imagem, windowname):
-		self.imagem = imagem
-		self.windowname = windowname
-		self.pontosx = [-1, -1]
-		self.pontosy = [-1, -1]
-
-# pontosx = [-1,-1]
-# pontosy = [-1,-1]
+    def __init__(self, imagem, windowname):
+        self.imagem = imagem
+        self.windowname = windowname
+        self.pontosx = [-1, -1]
+        self.pontosy = [-1, -1]
 
 def clickEvent(event, x, y, flags, janela):
-	# global pontosx,pontosy
-	if event == cv2.EVENT_LBUTTONDOWN:
-		janela.pontosx[1] = janela.pontosx[0]
-		janela.pontosx[0] = x
-		janela.pontosy[1] = janela.pontosy[0]
-		janela.pontosy[0] = y
-		# print ('Point 1: X = ',pontosy[1] ,'Y = ', pontosx[1])
-		# print ('Point 2: X = ',pontosy[0] ,'Y = ', pontosx[0])
-		if janela.pontosx[1]!=-1:
-			distEucl = math.sqrt((janela.pontosx[0]-janela.pontosx[1])**2 + (janela.pontosy[0]-janela.pontosy[1])**2)
-			print ("Pixel distance:",distEucl)
-			imagem2 = janela.imagem.copy()
-			cv2.line(imagem2,(janela.pontosx[0],janela.pontosy[0]),(janela.pontosx[1],janela.pontosy[1]),(0,0,255),4)
-			cv2.imshow(janela.windowname,imagem2)
-			janela.pontosx = [-1,-1]		# Não sei se assim fica melhor
-			janela.pontosy = [-1,-1]		
+    if event == cv2.EVENT_LBUTTONDOWN:
+        janela.pontosx[1] = janela.pontosx[0]
+        janela.pontosx[0] = x
+        janela.pontosy[1] = janela.pontosy[0]
+        janela.pontosy[0] = y
+
+        if janela.pontosx[1]!=-1:
+            distEucl = math.sqrt((janela.pontosx[0]-janela.pontosx[1])**2 + (janela.pontosy[0]-janela.pontosy[1])**2)
+            print ("Pixel distance:",distEucl)
+            imagem2 = janela.imagem.copy()
+            cv2.line(imagem2,(janela.pontosx[0],janela.pontosy[0]),(janela.pontosx[1],janela.pontosy[1]),(0,0,255),4)
+            cv2.imshow(janela.windowname,imagem2)
+            janela.pontosx = [-1,-1]
+            janela.pontosy = [-1,-1]		
 
 def main():
-	imagem = cv2.imread('data/a.jpg')
-	# imagem2 = imagem.copy()
-	cv2.imshow('Imagem',imagem)
-	janela1 = janela(imagem, 'Imagem')
-	cv2.setMouseCallback('Imagem', clickEvent, janela1)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    imagem = cv2.imread('data/a.jpg')
+    cv2.imshow('Imagem',imagem)
+    janela1 = janela(imagem, 'Imagem')
+    cv2.setMouseCallback('Imagem', clickEvent, janela1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__  == "__main__":
-	main()
+    main()
--- req2.py
+++ req2.py
@@ -3,62 +3,62 @@ import numpy as np
 import glob
 
 def main():
-	windowName = 'Janela'
-	board_h = 6
-	board_w = 8
-	board_sz = (board_w, board_h)
-	board_total = board_w * board_h
+    windowName = 'Janela'
+    board_h = 6
+    board_w = 8
+    board_sz = (board_w, board_h)
+    board_total = board_w * board_h
 
-	objp = np.zeros((6*8,3), np.float32)
-	objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)      # "Coordenadas do mundo real" do tabuleiro de xadrez
-	objp = 2.8 * objp     #--> Colocar o lado do quadrado parece não influenciar nada o resultado.
+    objp = np.zeros((6*8,3), np.float32)
+    objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)      # "Coordenadas do mundo real" do tabuleiro de xadrez
+    objp = 2.8 * objp     #--> Colocar o lado do quadrado parece não influenciar nada o resultado.
 
-	image_points = []
-	object_points = []
+    image_points = []
+    object_points = []
 
-	successes = 0
-	frame_names = []
-	images = glob.glob('calib/*.jpg')
+    successes = 0
+    frame_names = []
+    images = glob.glob('calib/*.jpg')
 
-	for fname in images:
-		frame_names.append(fname)
-		frame = cv2.imread(fname)
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		patternWasFound, corners = cv2.findChessboardCorners(frame, board_sz)
-		
-		if patternWasFound:         # Se achou o tabuleiro
-			frame_display = cv2.drawChessboardCorners(frame, board_sz, corners, patternWasFound)
+    for fname in images:
+        frame_names.append(fname)
+        frame = cv2.imread(fname)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        patternWasFound, corners = cv2.findChessboardCorners(frame, board_sz)
+        
+        if patternWasFound:         # Se achou o tabuleiro
+            frame_display = cv2.drawChessboardCorners(frame, board_sz, corners, patternWasFound)
 
-			if len(corners) == board_total:     # Se achou todos os pontos na imagem
-				image_points.append(corners)    # Coordenadas no espaço da imagem
-				object_points.append(objp)      # Coordenadas no mundo real (sempre o mesmo (?))
-				successes += 1
-			else: 
-				print("Este frame nao achou o padrao corretamente:",fname)
-		else:
-			print("Este frame nao achou o padrao:",fname)
-			frame_display = frame.copy()
+            if len(corners) == board_total:     # Se achou todos os pontos na imagem
+                image_points.append(corners)    # Coordenadas no espaço da imagem
+                object_points.append(objp)      # Coordenadas no mundo real (sempre o mesmo (?))
+                successes += 1
+            else: 
+                print("Este frame nao achou o padrao corretamente:",fname)
+        else:
+            print("Este frame nao achou o padrao:",fname)
+            frame_display = frame.copy()
 
-		if cv2.waitKey(25) & 0xFF == ord('q'): 
-			break
-		cv2.imshow(windowName, frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'): 
+            break
+        cv2.imshow(windowName, frame)
 
-	# Função que faz a calibração
-	retVal, intrinsics_matrix, distortion_matrix, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, frame.shape[::-1], None, None)
+    # Função que faz a calibração
+    retVal, intrinsics_matrix, distortion_matrix, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, frame.shape[::-1], None, None)
 
-	# Salva os .xml com os parâmetros do requisito 2
-	intrinsics_file = cv2.FileStorage('intrinsics.xml', flags = 1)
-	distortion_file = cv2.FileStorage('distortion.xml', flags = 1)
-	intrinsics_file.write(name = 'intrinsics', val = intrinsics_matrix)
-	distortion_file.write(name = 'distortion', val = distortion_matrix)
-	intrinsics_file.release()
-	distortion_file.release()
+    # Salva os .xml com os parâmetros do requisito 2
+    intrinsics_file = cv2.FileStorage('intrinsics.xml', flags = 1)
+    distortion_file = cv2.FileStorage('distortion.xml', flags = 1)
+    intrinsics_file.write(name = 'intrinsics', val = intrinsics_matrix)
+    distortion_file.write(name = 'distortion', val = distortion_matrix)
+    intrinsics_file.release()
+    distortion_file.release()
 
-	cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
-	print("Distances from camera to pattern:")
-	for i in range(successes):
-		print(frame_names[i],": ",tvecs[i][2][0],", ",rvecs[i][2][0],sep="")
+    print("Distances from camera to pattern:")
+    for i in range(successes):
+        print(frame_names[i],": ",tvecs[i][2][0],", ",rvecs[i][2][0],sep="")
 
 if __name__  == "__main__":
-	main()
+    main()
