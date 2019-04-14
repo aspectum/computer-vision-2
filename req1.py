eb1600d54323cc7799a0c01556d9da667a0ef3e4
aspectum <drogpe@gmail.com>
2019-04-14 17:09:06 -0300

    Modificado req1 para adicionar compatibilidade

--- req1.py
+++ req1.py
@@ -2,30 +2,40 @@ import cv2
 import numpy as np
 import math
 
-pontosx = [-1,-1]
-pontosy = [-1,-1]
+class janela:
+	def __init__(self, imagem, windowname):
+		self.imagem = imagem
+		self.windowname = windowname
+		self.pontosx = [-1, -1]
+		self.pontosy = [-1, -1]
 
-def clickEvent(event, x, y, flags, imagem):
-	global pontosx,pontosy
+# pontosx = [-1,-1]
+# pontosy = [-1,-1]
+
+def clickEvent(event, x, y, flags, janela):
+	# global pontosx,pontosy
 	if event == cv2.EVENT_LBUTTONDOWN:
-		pontosx[1] = pontosx[0]
-		pontosx[0] = x
-		pontosy[1] = pontosy[0]
-		pontosy[0] = y
+		janela.pontosx[1] = janela.pontosx[0]
+		janela.pontosx[0] = x
+		janela.pontosy[1] = janela.pontosy[0]
+		janela.pontosy[0] = y
 		# print ('Point 1: X = ',pontosy[1] ,'Y = ', pontosx[1])
 		# print ('Point 2: X = ',pontosy[0] ,'Y = ', pontosx[0])
-		if pontosx[1]!=-1:
-			distEucl = math.sqrt((pontosx[0]-pontosx[1])**2 + (pontosy[0]-pontosy[1])**2)
+		if janela.pontosx[1]!=-1:
+			distEucl = math.sqrt((janela.pontosx[0]-janela.pontosx[1])**2 + (janela.pontosy[0]-janela.pontosy[1])**2)
 			print ("Pixel distance:",distEucl)
-			imagem2 = imagem.copy()
-			cv2.line(imagem2,(pontosx[0],pontosy[0]),(pontosx[1],pontosy[1]),(0,0,255),4)
-			cv2.imshow('Imagem',imagem2)		
+			imagem2 = janela.imagem.copy()
+			cv2.line(imagem2,(janela.pontosx[0],janela.pontosy[0]),(janela.pontosx[1],janela.pontosy[1]),(0,0,255),4)
+			cv2.imshow(janela.windowname,imagem2)
+			janela.pontosx = [-1,-1]		# Não sei se assim fica melhor
+			janela.pontosy = [-1,-1]		
 
 def main():
 	imagem = cv2.imread('data/a.jpg')
-	imagem2 = imagem.copy()
-	cv2.imshow('Imagem',imagem2)
-	cv2.setMouseCallback('Imagem', clickEvent, imagem)
+	# imagem2 = imagem.copy()
+	cv2.imshow('Imagem',imagem)
+	janela1 = janela(imagem, 'Imagem')
+	cv2.setMouseCallback('Imagem', clickEvent, janela1)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
